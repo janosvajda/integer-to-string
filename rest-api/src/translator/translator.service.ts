20a201f94b6eeef8c75ccfc4b82e4a7b3a4024de
janosvajda <vajdajanos@gmail.com>
2022-08-14 06:25:11 +0100

    Tranaslator microservice initial version has been created and configured for receiving messgaes.

--- rest-api/src/translator/translator.service.ts
+++ rest-api/src/translator/translator.service.ts
@@ -7,12 +7,10 @@ export class TranslatorService {
   private readonly result: TranslatorEntity[] = [];
 
   translate(data: number): TranslatorEntity {
-    console.log('FASZ', data);
     return this.result[data];
   }
 
   ssss(data: number): TranslatorEntity {
-    console.log(data);
     return this.result[data];
   }
 }
--- translate-service/src/app.controller.ts
+++ translate-service/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import {
+  Ctx,
+  KafkaContext,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('translate.data')
+  readMessage(@Payload() message: any, @Ctx() context: KafkaContext) {
+    const originalMessage = context.getMessage();
+    const response =
+      `Receiving a new message that should be processed: ` +
+      JSON.stringify(originalMessage.value);
+    console.info(response);
+    return response;
   }
 }
