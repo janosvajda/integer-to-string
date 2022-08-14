ff4f334bad25e5c6207ed4a510f8fb9ea81efcb2
janosvajda <vajdajanos@gmail.com>
2022-08-14 01:48:21 +0100

    Hard coded port numbers have  been changed. Configuration should come from .env files.

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TranslatorModule } from './translator/translator.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TranslatorModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TranslatorModule,
+  ],
 })
 export class AppModule {}
