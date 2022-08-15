eabfee190951223160eff1ff0d1855bd4307beb4
janosvajda <vajdajanos@gmail.com>
2022-08-15 05:50:30 +0100

    Code cleaning in REST Api part.

--- rest-api/src/translator/translator.service.ts
+++ rest-api/src/translator/translator.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { TranslatorEntity } from './dto/translator.entity';
-import { GetTranslatorDto } from './dto/get-translator.dto';
 
 @Injectable()
 export class TranslatorService {
   private readonly result: TranslatorEntity[] = [];
 
   translate(data: number): TranslatorEntity {
-    return this.result[data];
-  }
-
-  ssss(data: number): TranslatorEntity {
     return this.result[data];
   }
 }
